f9e2abde7badde00fe054e31b7d9243a56282554
kusum278 <85170195+kusum278@users.noreply.github.com>
2021-08-05 00:53:24 +0530

    day 3 submitted

---
+++ day3/day3.js
@@ -0,0 +1,6 @@
+function namecity(){
+let name=  document.querySelector("#name").value;
+let city=  document.querySelector("#city").value;
+document.querySelector(".nc").innerHTML=`<h1>Name: ${name} City: ${city} </h1>`;
+
+}
